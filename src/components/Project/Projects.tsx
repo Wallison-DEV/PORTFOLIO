ef786c45191db5b983519602f126c70536f911ba
Wallison <wallisonzwka@gmail.com>
2024-03-29 01:16:38 -0300

    add_projects

--- src/components/Project/Projects.tsx
+++ src/components/Project/Projects.tsx
@@ -21,7 +21,7 @@ type ProjectProp = {
 const ProjectsList: ProjectProp[] = [
     {
         src: './src/assets/images/eplay.png',
-        data: 'Apr 2024',
+        data: 'Mar 2024',
         nome: 'EPLAY',
         descricao:
             'Este é um projeto de loja de jogos digitais desenvolvido com React, TypeScript e outras tecnologias relacionadas como : integração com API externa para obter dados dos jogos usando createApi do Redux Toolkit, navegação entre seções utilizando React Router, gerenciamento de estado centralizado com Redux, validação de formulários utilizando Yup e Formik .A aplicação consiste em várias páginas, incluindo: Página Inicial, Categorias, Produto, Checkout ',
@@ -29,22 +29,40 @@ const ProjectsList: ProjectProp[] = [
         link2: 'https://eplay-beta.vercel.app/',
     },
     {
-        src: './src/assets/images/django_blog.png',
-        data: 'Jan 2024',
-        nome: 'Django Blog',
+        src: './src/assets/images/weather.png',
+        data: 'Fev 2024',
+        nome: 'Weather Wise',
         descricao:
-            'Este é um projeto de blog desenvolvido utilizando Django, Bootstrap e outras tecnologias relacionadas. A aplicação é composta por várias páginas, incluindo a página inicial que exibe todas as postagens, a página de detalhes que mostra uma postagem com mais informações e a página de cadastro de novas postagens.',
-        link: 'https://github.com/Wallison-DEV/django_blog.git',
-        link2: 'https://eplay-beta.vercel.app/',
+            'O "WeatherWise" é uma aplicação web que fornece informações meteorológicas em tempo real para diversas cidades. Com uma interface responsiva e amigável, os usuários podem buscar o clima atual e a previsão do tempo para os próximos dias. Desenvolvido com HTML, CSS e JavaScript, o aplicativo utiliza a API do OpenWeatherMap para obter dados precisos e atualizados. Ideal para quem deseja se manter informado sobre as condições climáticas.',
+        link: 'https://github.com/Wallison-DEV/wather_wise',
+        link2: 'https://weather-wise-gules.vercel.app/',
     },
     {
         src: './src/assets/images/maps.png',
-        data: 'Feb 2024',
+        data: 'Fev 2024',
         nome: 'Map Search',
         descricao:
             'Este projeto é um aplicativo de mapas desenvolvido de forma autônoma, sem a assistência de tutores. Utilizando a API do Google Maps, o aplicativo proporciona funcionalidades avançadas de visualização e interação com mapas. Seu principal objetivo é criar uma plataforma que permita aos usuários explorar mapas de forma intuitiva, além de visualizar informações geográficas e interagir com pontos de interesse. A aplicação foi desenvolvida com tecnologias web padrão, incluindo HTML, CSS, JavaScript e Ajax.',
         link: 'https://github.com/Wallison-DEV/maps',
         link2: 'https://mapsearch.vercel.app/',
+    },
+    {
+        src: './src/assets/images/todo.png',
+        data: 'Mar 2024',
+        nome: 'To Do List',
+        descricao:
+            'Este projeto é uma aplicação de lista de tarefas (To Do List) desenvolvida com Django, Bootstrap e outras tecnologias. Ele apresenta diversas páginas, como a página inicial que lista todas as tarefas, a página de detalhes para informações adicionais sobre uma tarefa e a página de cadastro de novas tarefas.',
+        link: 'https://github.com/Wallison-DEV/todo_django',
+        link2: 'https://github.com/Wallison-DEV/todo_django',
+    },
+    {
+        src: './src/assets/images/django_blog.png',
+        data: 'Jan 2024',
+        nome: 'Personal Blog',
+        descricao:
+            'Este é um projeto de blog desenvolvido utilizando Django, Bootstrap e outras tecnologias relacionadas. A aplicação é composta por várias páginas, incluindo a página inicial que exibe todas as postagens, a página de detalhes que mostra uma postagem com mais informações e a página de cadastro de novas postagens.',
+        link: 'https://github.com/Wallison-DEV/django_blog.git',
+        link2: 'https://github.com/Wallison-DEV/django_blog.git',
     },
 ]
 
@@ -97,6 +115,18 @@ const Projects = () => {
                     <CarouselPrevious />
                     <CarouselNext />
                 </CenteredCarousel>
+                <div className="flex justify-center md:mt-12">
+                    <Button className="flex items-center w-80 bg-blue-600 hover:bg-blue-500">
+                        <a
+                            href="https://vercel.com/wallisondevs"
+                            className="text-white text-center w-full"
+                            target="_blank"
+                            rel="noopener noreferrer"   
+                        >
+                            Ver mais projetos
+                        </a>
+                    </Button>
+                </div>
             </div>
         </Container>
     )
