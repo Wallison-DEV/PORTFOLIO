fe0df7a484f9e447e5f8f939c889843830d517ea
wallison_DEV <130496619+Wallison-DEV@users.noreply.github.com>
2026-03-25 12:14:16 -0300

    Update next.config.js configuration settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  swcMinify: true,
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
